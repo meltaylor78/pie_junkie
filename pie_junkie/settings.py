4625c32ab6881961dd35276877778116f63c1014
meltaylor78 <meltaylor78@gmail.com>
2021-06-30 20:54:26 +0000

    Added base structure to the Base templates and home app index template. Added blocks to structure the templates and extensions. Created Urls file for home app and added route for index template. Added home app and base DIRs to stetting.py file. Tested to ensure the templates loaded.

--- pie_junkie/settings.py
+++ pie_junkie/settings.py
@@ -3,6 +3,7 @@ Django settings for pie_junkie project.
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,6 +34,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home'
 ]
 
 MIDDLEWARE = [
@@ -50,7 +52,10 @@ ROOT_URLCONF = 'pie_junkie.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
