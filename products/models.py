ef15d4108a9d8ae4ab9298eb53320fd6d37b52b3
meltaylor78 <meltaylor78@gmail.com>
2021-07-05 20:04:19 +0000

    updated the fields in products model to include power and ram, this will alow site take input for type of plug (EU, UK, US) and the ram in the Pi if the flags are set to yes

--- products/models.py
+++ products/models.py
@@ -19,8 +19,9 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    power = models.BooleanField(max_length=254, null=True, blank=True)
+    ram = models.BooleanField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
