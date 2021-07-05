cc60a456964e0602ac6ac90b57f687638f9d88d5
meltaylor78 <meltaylor78@gmail.com>
2021-07-05 20:16:29 +0000

    updated admin view to add addtional display items in the category and product views in admin view

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from .models import Product, Category
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display =(
+        'sku',
+        'name',
+        'category',
+        'image',
+        'power',
+        'ram',
+    )
+
+    ordering = ('category',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display =(
+        'friendly_name',
+        'name'
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
