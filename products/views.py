5cc4714100070b1582a68c77bbcad5c3668aef6a
meltaylor78 <meltaylor78@gmail.com>
2021-07-05 20:22:02 +0000

    imported models and set-up context

--- products/views.py
+++ products/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from .models import Product
 # products view
 
 def all_products(request):
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
     return render(request, 'products/products.html', context)
