be0d1c70901e450531f24d661ada483d6d5c3ca4
meltaylor78 <meltaylor78@gmail.com>
2021-07-13 22:07:37 +0000

    updated shopping cart page to display products in the card. working on adding the ram and power options to the system. Issue currently with the options not being pulled in with the post method. Working on the issue, will return to it tomorrw

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,11 +1,36 @@
+from django.shortcuts import get_object_or_404
 from decimal import Decimal
 from django.conf import settings
+from products.models import Product
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, item_data in cart.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            cart_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for ram, quantity in item_data['ram_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                cart_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'ram': ram,
+                })
 
     if total < settings.FREE_DELIVERY:
         delivery = total * Decimal(settings.DELIVERY_PERCENT / 100)
--- cart/views.py
+++ cart/views.py
@@ -7,13 +7,33 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    ram = None
+    power = None
+
+    if 'product_ram' in request.POST:
+      
+        ram = request.POST['product_ram']
+      
+
+    if 'product_power' in request.POST:
+  
+        power = request.POST['product_power']
+
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    if ram:
+        if item_id in list(cart.keys()):
+            if ram in cart[item_id]['ram_size'].keys():
+                cart[item_id]['ram_size'][ram] += quantity
+            else:
+                cart[item_id]['ram_size'][ram] = quantity
+        else:
+            cart[item_id] = {'ram_size': {ram: quantity}}
     else:
-        cart[item_id] = quantity
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+        else:
+            cart[item_id] = quantity
 
     request.session['cart'] = cart
-    print(request.session['cart'])
     return redirect(redirect_url)
