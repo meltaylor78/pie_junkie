ed5fbaa8e457dba8d6a18314204bf1d7d3e63db6
meltaylor78 <meltaylor78@gmail.com>
2021-08-01 20:29:34 +0000

    adding set-up fopr stripe webhooks

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-
+from .webhooks import webhook
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_successful/<order_number>', views.checkout_success, name='checkout_successful'),
+    path('wh/', webhook, name='webhook')
 ]
