0e79deb17a6ebbd51779e9214ea86defe6a6ac1d
meltaylor78 <meltaylor78@gmail.com>
2021-08-01 19:22:06 +0000

    removed message for succesful order from views as it is duplicating whats is on the main page. Updated content on order succesful page

--- checkout/views.py
+++ checkout/views.py
@@ -120,9 +120,6 @@ def checkout(request):
 def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(request, f'Order successfully processed! \
-        Your order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.')
 
     if 'cart' in request.session:
         del request.session['cart']
