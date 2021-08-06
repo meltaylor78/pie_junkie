4b351d7cfec847074fd68ac86bff005ca6e9f529
meltaylor78 <meltaylor78@gmail.com>
2021-08-06 22:54:00 +0000

    fixed a typo in settings

--- pie_junkie/settings.py
+++ pie_junkie/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['pie-junkie.herokuapp.comclear','localhost']
+ALLOWED_HOSTS = ['pie-junkie.herokuapp.com','localhost']
 
 
 # Application definition
