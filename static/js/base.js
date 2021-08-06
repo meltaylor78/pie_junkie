e8166c547a9b1c75018c4bd10ae11902c8d57df4
meltaylor78 <meltaylor78@gmail.com>
2021-08-06 18:06:01 +0000

    fixed issue in the where the shopping cart total from the mobile header was showing up on larger devices. Updated and working ok now

---
+++ static/js/base.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $("#product-search-large").submit(function (e) {
+        e.preventDefault();
+        let search = $("#custom-search-group-large").val();
+        window.location = `/products/?search=${search}`;
+    });
+
+    $("#product-search-small").submit(function (e) {
+        e.preventDefault();
+        let search = $("#custom-search-group-small").val();
+        window.location = `/products/?search=${search}`;
+    });
+
+    $(".close-message-container").click(function() {
+        $(".message-container").hide();
+    });
+
+    setTimeout(function() {
+        $(".message-container").hide();
+    }, 4000);
+});
